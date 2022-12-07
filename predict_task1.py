f48957ec6f2b57d92e7ed0a891c89b2babc251e5
Yang <nandayanger@gmail.com>
2022-12-07 16:27:40 +0000

    Update predict_task1.py

--- predict_task1.py
+++ predict_task1.py
@@ -13,14 +13,15 @@ for case in list_case:
     img = sitk.ReadImage(path_img.format(case))
 
     ##
-    # your logic here. Below we do binary thresholding as a demo
-    ##
+    # =======your logic here. Below we do binary thresholding as a demo=====
 
     # using SimpleITK to do binary thresholding between 100 - 10000
     vs_pred = sitk.BinaryThreshold(img, lowerThreshold=400, upperThreshold=500)
     cochlea_pred = sitk.BinaryThreshold(img, lowerThreshold=900, upperThreshold=1100)
 
     result = vs_pred + 2*cochlea_pred
-
+    # ======================================================================
+    # please make sure the results were processed with largest component extraction
+    result = sitk.GetImagefromArray(result)
     # save the segmentation mask
     sitk.WriteImage(result, path_pred.format(case))
